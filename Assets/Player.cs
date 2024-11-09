bc6df47b66df70e0a3217857918735833cbe7281
machidamachio <wd.machida@gmail.com>
2024-11-09 11:29:49 +0900

    11/9

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]//エディタでできるらしいよ
+    private float jumpPower = 4;//ジャンプ力
+
+    [SerializeField]
+    private float moveSpped = 10;//移動
+
+    private CharacterController CharacterController;
+    private Vector3 velocity;//
+
+    public floa
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
