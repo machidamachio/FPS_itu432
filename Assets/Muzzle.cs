2cd2b56fe2cf1d2275138a0a1ff98df806b01ef4
machidamachio <wd.machida@gmail.com>
2024-12-07 11:27:45 +0900

    12/7

---
+++ Assets/Muzzle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muzzle : MonoBehaviour
+    //どこらアクセス
+    public GameObject bulletPrefab;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    if(Input.GetMouseButtonDown(0)){
+        short();
+    }
+
+    //関数
+    void Shot(){
+        //gameObject形の[ブレット]
+        GameObject bullet;
+        //
+        bullet = GameObject.Instantiate(bulletPrefab);
+        //
+        bullet.transform.position = transform.position;
+        //
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+    }
+    
+}   
