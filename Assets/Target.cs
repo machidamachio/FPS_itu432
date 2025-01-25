0b75f72958c6bc36ce9a0482f6ae2b41a98d02aa
machidamachio <wd.machida@gmail.com>
2025-01-25 11:28:37 +0900

    1/25_2

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;//
 
 public class Target : MonoBehaviour
 {
+    public GameObject target;//
+    UnityEngine.AI.NavMeshAgent agent;//
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();//
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = target.transform.position;//agentの目的地
     }
 
     //どこからアクセス
